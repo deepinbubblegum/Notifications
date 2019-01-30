b09667e43476a15f55aa313cea0ccbf14632c438
deepinbubblegum <5911110222@mutacth.com>
2019-01-30 11:00:51 +0700

    add

--- app/src/main/java/com/deepin/notifications/MainActivity.java
+++ app/src/main/java/com/deepin/notifications/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     private Button button_notify;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_stat_name);
+        return notifyBuilder;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification();
             }
         });
+        createNotificationChannel();
     }
 
     public void createNotificationChannel() {
@@ -44,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendNotification() {
-
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 }
